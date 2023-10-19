537c02141067cfccd123416999522426ec5b476b
Dima Kushchenko <dimon077@yandex.ru>
2023-10-19 12:46:28 +0300

    solved 4-module 3-task

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,23 @@
 function highlight(table) {
-  // ваш код...
+  let rows = table.querySelectorAll('tbody > tr');
+
+  for (let tr of rows) {
+    let dots = tr.querySelectorAll('td');
+
+    if (dots[3].hasAttribute('data-available')) {
+      dots[3].dataset.available == "true" ? tr.classList.add('available') : tr.classList.add('unavailable');
+    } else {
+      tr.hidden = true;
+    }
+
+    if (dots[2].innerHTML == 'm') {
+      tr.classList.add('male');
+    } else if (dots[2].innerHTML == 'f') {
+      tr.classList.add('female');
+    }
+
+    if (dots[1].innerHTML < 18) {
+      tr.style.textDecoration = 'line-through';
+    }
+  }
 }
