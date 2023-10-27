d9bf40780a754aee4aa775ee9f7dfe91c7589953
Dima Kushchenko <dimon077@yandex.ru>
2023-10-27 17:28:24 +0300

    solved 6-module 1-task

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,20 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.elem = document.createElement('table');
+
+    this.elem.insertAdjacentHTML('beforeEnd', '<thead><tr><th>Имя</th><th>Возраст</th><th>Зарплата</th><th>Город</th><th></th></tr></thead><tbody></tbody>');
+
+    this.elem.querySelector('tbody').insertAdjacentHTML(
+      'beforeEnd',
+      rows.map(({ name, age, salary, city }) => `<tr><td>${name}</td><td>${age}</td><td>${salary}</td><td>${city}</td><td><button>X</button></td></tr>`).join(''));
+
+    this.elem.addEventListener('click', this.removeOnClick);
+  }
+
+  removeOnClick = (event) => {
+    if (event.target.closest('button')) {
+      event.target.closest('tr').remove();
+    }
   }
 }
