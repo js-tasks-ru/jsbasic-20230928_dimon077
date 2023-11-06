04c7f17e1f55ccfebdc38d5171ada624f78803f3
Dima Kushchenko <dimon077@yandex.ru>
2023-11-06 12:59:35 +0300

    solved 8-module 2-task

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -5,5 +5,40 @@ export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
+    this.elem = createElement(`
+      <div class="products-grid">
+        <div class="products-grid__inner"></div>
+      </div>
+    `);
+    this.productsInner = this.elem.querySelector('.products-grid__inner');
+
+    this.productsCards = this.products.map(product => Object.assign((new ProductCard(product)), { spiciness: product.spiciness, nuts: product.nuts, vegeterian: product.vegeterian }));
+
+    this.addCards(this.productsCards);
+  }
+
+  addCards = (cards) => {
+    for (let card of cards) {
+      this.productsInner.append(card.elem);
+    }
+  }
+
+  updateFilter = (newFilter) => {
+    Object.assign(this.filters, newFilter);
+    let { noNuts = false, vegeterianOnly = false, maxSpiciness = 4, category = '' } = this.filters;
+    let filteredCards = this.productsCards.filter(e => e.spiciness <= maxSpiciness);
+
+    if (noNuts) {
+      filteredCards = filteredCards.filter(e => !e.nuts);
+    }
+    if (vegeterianOnly) {
+      filteredCards = filteredCards.filter(e => e.vegeterian);
+    }
+    if (category) {
+      filteredCards = filteredCards.filter(e => e.category == category);
+    }
+
+    this.productsInner.innerHTML = '';
+    this.addCards(filteredCards);
   }
 }
